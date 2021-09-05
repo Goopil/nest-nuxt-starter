e8198df6f529e30584fcb428c9bae8b003c8b8a8
Zachary Volpi <zachary.volpi@infomaniak.com>
2021-09-05 20:46:46 +0200

    add default lang to html. thanks lighthouse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,7 @@ export default {
    */
   head: {
     title: 'title',
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
