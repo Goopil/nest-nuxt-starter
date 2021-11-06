ce83ff92c5f29aeeb84f0534658c9a91195d1967
Zachary Volpi <zachary.volpi@infomaniak.com>
2021-11-06 10:00:53 +0100

    use a more up to date nginx base image. and add ability to specify the hostname via env vars

--- proxy/start.sh
+++ proxy/start.sh
@@ -5,8 +5,8 @@ set -e
 
 mkdir -p /etc/nginx/conf.d/
 
-envsubst '${BACKEND_HOST}' < /template/proxy.nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_HOST},${HOST_NAME}' < /template/proxy.nginx.conf > /etc/nginx/conf.d/default.conf
 
-echo "back set to $BACKEND_HOST. Starting";
+echo "back set to $BACKEND_HOST with $HOST_NAME. Starting";
 
 nginx -g 'daemon off;'
