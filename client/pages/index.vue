b9bf9b92b67fd2916b761c3982c823bf41db26bd
Zachary Volpi <zachary.volpi@infomaniak.com>
2021-12-04 11:16:04 +0100

    add refresh btn to show that the client ajax call work properly

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>index</h1>
+    <button @click="fetchUsers()" :disabled="loading">refresh</button>
     <ul class="user-list">
       <li v-for="(user, i) in users" :key="i">
         <span>{{ user.name }}</span>
@@ -17,15 +18,26 @@
 </style>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-import { User } from '@common/UserInterface';
+import {User} from '@common/UserInterface';
+import {Component, Vue} from 'nuxt-property-decorator';
 
 @Component
 export default class YourComponent extends Vue {
   users: User[] = [];
+  loading: boolean = false;
 
   async fetch() {
-    this.users = (await this.$axios.get('users'))?.data ?? [];
+    return this.fetchUsers();
+  }
+
+  async fetchUsers() {
+    try {
+      this.loading = true;
+
+      this.users = (await this.$axios.get('users'))?.data ?? [];
+    } finally {
+      this.loading = false;
+    }
   }
 }
 </script>
