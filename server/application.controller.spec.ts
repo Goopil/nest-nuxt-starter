c87c395fc58002da165115675c63a4a6e149857f
Zachary Volpi <hello@zvolpi.dev>
2022-03-12 09:34:27 +0100

    add dummy test file to use yarn test

---
+++ server/application.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import {ApplicationController} from './application.controller'
+
+describe('ApplicationController', () => {
+  let controller: ApplicationController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ApplicationController],
+    }).compile();
+
+    controller = module.get<ApplicationController>(ApplicationController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
