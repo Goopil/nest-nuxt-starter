2fe12087a143a94489b435b0697c67fe7e752fd4
Zachary Volpi <hello@zvolpi.dev>
2022-07-03 10:35:23 +0200

    Add typing to request methods

--- server/nuxt/req.helpers.ts
+++ server/nuxt/req.helpers.ts
@@ -1,17 +1,17 @@
-function acceptJson(headers) {
+function acceptJson(headers: Record<string, string>): boolean {
   return ['/json', '+json'].some((segment) =>
     (headers.accept || '').includes(segment),
   );
 }
 
-function isXmlHttpRequest(headers) {
+function isXmlHttpRequest(headers: Record<string, string>): boolean {
   return headers['X-Requested-With'] === 'XMLHttpRequest';
 }
 
-function isPjax(headers) {
+function isPjax(headers: Record<string, string>): boolean {
   return !!headers['X-PJAX'];
 }
 
-export function jsonRequest(headers) {
+export function jsonRequest(headers: Record<string, string>): boolean {
   return [acceptJson, isXmlHttpRequest, isPjax].some((test) => test(headers));
 }
